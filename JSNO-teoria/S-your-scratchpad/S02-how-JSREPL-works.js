245f04bbf171fea1538fe86f7fc314e593bc5f6c
Jabi <michal.jabi@gmail.com>
2025-06-11 11:27:18 +0200

    explain(S02): how JSREPL/Quokka extension works.

---
+++ JSNO-teoria/S-your-scratchpad/S02-how-JSREPL-works.js
@@ -0,0 +1,22 @@
+console.log('start....')
+
+function sayHello() {
+    console.log('!')
+}
+
+try {
+    hej
+    console.log('?')
+    console.log('?')
+    console.log('?')
+    console.log('?')
+    console.log('?')
+} catch (e) {
+    console.log(e.message)
+}
+
+sayHello()
+sayHello()
+sayHello()
+sayHello()
+sayHello()
