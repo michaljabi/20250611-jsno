dd523c09473eb0ecc38a4f304a83765188204238
Jabi <michal.jabi@gmail.com>
2025-06-12 09:28:20 +0200

    task(00): solved -> explain PROVIDER / CONSUMER concept.

--- JSNO-teoria/00-test-your-might/00-callback-intro.js
+++ JSNO-teoria/00-test-your-might/00-callback-intro.js
@@ -1,7 +1,18 @@
 
+// @ts-check
+
 console.log('Hello stranger... shall we start?')
 
+
+// PROVIDER: 
+
 function giveMeTheNumber(mistery) {
+    // mistery(300)
+    setTimeout(() => {
+        mistery(1300)
+    }, 3000)
+
+    mistery(300)
     mistery(300)
 }
 
@@ -11,3 +22,22 @@ function giveMeTheNumber(mistery) {
  * Poniżej odbierz liczbę 300 i pokaż ją na ekranie:
  * */
 
+// CONSUMER:
+
+// #1
+giveMeTheNumber((value) => {
+    console.log(value);
+});
+
+
+// #2
+giveMeTheNumber((value) => {
+    console.log(value);
+});
+
+// #n...
+
+
+setTimeout(() => {
+    console.log('!')
+}, 8000)
