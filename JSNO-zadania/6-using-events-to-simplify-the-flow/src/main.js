1b7380f5e19f02484f9a71141714e8d42a28a282
Jabi <michal.jabi@gmail.com>
2025-06-12 15:24:48 +0200

    feat(task-6): make it work

--- JSNO-zadania/6-using-events-to-simplify-the-flow/src/main.js
+++ JSNO-zadania/6-using-events-to-simplify-the-flow/src/main.js
@@ -1,3 +1,6 @@
+import { stdin as input, stdout as output } from 'node:process'
+import { createInterface } from 'node:readline/promises'
+
 import { programEventBus } from "./program-events.js";
 
 
@@ -15,9 +18,28 @@ Obsługa programu:
   2. wpisz: koniec - aby zakończyć działanie programu`)
 })
 
-//
+programEventBus.on('onClose', () => {
+    console.log(`---
+Żegnaj.`)
+});
+
+const rl = createInterface({ input, output });
+
 programEventBus.on('onLoad', async () => {
-    const aswer = await rl.question('Podaj ścieżkę: ')
+    const answer = await rl.question('Podaj ścieżkę: ')
+    if (answer === 'koniec') {
+        rl.close();
+        return programEventBus.emit('onClose')
+    }
+    programEventBus.emit('onPathSegmentsRequested', answer)
 })
+
+programEventBus.on('onPathSegmentsRequested', path => {
+    const segments = path.split('/');
+    console.log(`- Liczba segmentów ${segments.length}
+- Segmenty: [${segments}]`)
+    programEventBus.emit('onLoad')
+})
+
 
 programEventBus.emit('onStart')
