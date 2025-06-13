4613f626e5ff7ccc2b24464b023e42e94f5a1535
Jabi <michal.jabi@gmail.com>
2025-06-13 11:21:20 +0200

    refactor(task-9): make it right

--- JSNO-zadania/9-simple-http-server/src/main.js
+++ JSNO-zadania/9-simple-http-server/src/main.js
@@ -9,20 +9,35 @@ console.log(data.products.length);
 const { PORT } = env;
 
 const server = createServer((request, response) => {
-    console.log("Method", request.method);
-    console.log("URL", request.url);
-    console.log("Headers:", request.headers);
+    // console.log("Method", request.method);
+    // console.log("URL", request.url);
+    // console.log("Headers:", request.headers);
     // const { host } = request.headers;
 
-    switch (request.url) {
-        case "/products":
-            response.write(JSON.stringify(data.products));
-            break;
-        case "/shop-info":
-            response.write(data.shopName);
-            break;
-    }
+    const { products, shopName } = data;
+    const { method, url } = request;
 
+    if (method === 'GET') {
+        switch (url) {
+            case "/products":
+                response.setHeader('Content-Type', 'application/json')
+                response.write(JSON.stringify(products));
+                break;
+            case "/shop-info":
+                response.setHeader('Content-Type', 'application/json')
+                response.write(JSON.stringify({ shopName }));
+                break;
+            default:
+                // to też powinno być produkcyjnie jako `JSON` i zmienione na jakiś wysyłany obiekt...
+                response.setHeader('Content-Type', 'text/plain')
+                response.statusCode = 404;
+                response.write(`404 - nie znam ścieżki ${url}`)
+        }
+    } else {
+        response.setHeader('Content-Type', 'application/json')
+        response.statusCode = 400;
+        response.write(JSON.stringify({ error: `Method ${method} not supported...` }))
+    }
     //response.statusCode = 404;
     //response.write(`Hello world? You are: ${host}`);
     response.end();
