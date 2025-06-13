75ea5ac8f0e1d4326975670e33778b4e64b6f446
Jabi <michal.jabi@gmail.com>
2025-06-13 11:00:22 +0200

    explain(task-9): `npm i prettier -D` + format script.

--- JSNO-zadania/9-simple-http-server/src/main.js
+++ JSNO-zadania/9-simple-http-server/src/main.js
@@ -1,22 +1,28 @@
-import { env } from 'node:process'
-import { createServer } from 'node:http'
+import { env } from "node:process";
+import { createServer } from "node:http";
+
+import data from "../data.json" with { type: "json" };
+
+console.log(data.shopName);
+console.log(data.products.length);
 
 const { PORT } = env;
 
 const server = createServer((request, response) => {
+  console.log("Method", request.method);
+  console.log("URL", request.url);
+  console.log("Headers:", request.headers);
+  const { host } = request.headers;
 
-    console.log('URL', request.url)
-    console.log('Headers:', request.headers)
-    const { host } = request.headers;
-
-    response.statusCode = 404
-    response.write(`Hello world? You are: ${host}`);
-    response.end();
-})
+  response.statusCode = 404;
+  response.write(`Hello world? You are: ${host}`);
+  response.end();
+  // response.write(`dopisane....`);
+});
 
 server.listen(PORT, () => {
-    console.log(`Server http://localhost:${PORT} running..`)
-})
+  console.log(`Server http://localhost:${PORT} running..`);
+});
 
 // console.log(process.env.PORT)
-// console.log(process.env.EDGE_CASE) 
+// console.log(process.env.EDGE_CASE)
